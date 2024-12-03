7482e5869b2d7402849a753c9d0c4ea31bf26f88
briskaanandaa <briskaananda103@gmail.com>
2024-12-03 14:45:40 +0700

    Memisahkan Sidebar User dan Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import { Sidebar, SidebarItem } from "./components/sidebar";
-import {
-  FiBell,
-  FiCreditCard,
-  FiFile,
-  FiGrid,
-  FiHome,
-  FiLogOut,
-  FiMessageSquare,
-  FiPackage,
-  FiThumbsUp,
-  FiUsers,
-} from "react-icons/fi";
+import SidebarUser from "./components/SidebarUser";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Sidebar>
-        <SidebarItem icon={<FiGrid />} text="Dashboard" route="/" />
-        <SidebarItem icon={<FiUsers />} text="Pengguna" />
-        <SidebarItem icon={<FiPackage />} text="Penitipan" />
-        <SidebarItem icon={<FiCreditCard />} text="Pembayaran" />
-        <SidebarItem icon={<FiFile />} text="Riwayat" active />
-        <SidebarItem icon={<FiThumbsUp />} text="Ulasan" />
-        <SidebarItem icon={<FiBell />} text="Notifikasi" />
-        <SidebarItem icon={<FiMessageSquare />} text="Chat" />
-        <SidebarItem icon={<FiHome />} text="Beranda" beranda />
-        <SidebarItem icon={<FiLogOut />} text="Keluar" keluar />
-      </Sidebar>
+      <SidebarUser />
     </>
   );
 }
