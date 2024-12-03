8292b1ae8ca60b38a6fddbb31d20a90db8d9eaa0
Alia <absentugasalia@gmail.com>
2024-12-03 14:06:25 +0700

    feat:component header admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import HeaderUser from './components/headerUser';
+import HeaderAdmin from './components/headerAdmin';
 import './index.css';
 
 function App() {
   return (
     <div>
       <HeaderUser />
+
+      <HeaderAdmin />
+
       <main>
         {/* Konten lainnya */}
       </main>
