ac7ffcccdbca009682beb110e7085c03b14b7c46
briskaanandaa <briskaananda103@gmail.com>
2024-12-11 20:20:01 +0700

    Menambahkan id transaction

--- backend/src/models/transaction_model.js
+++ backend/src/models/transaction_model.js
@@ -1,22 +1,38 @@
 const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  warehouse_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Warehouse', required: true },
-  barang_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Barang', required: true },
-  titik_alamat: { type: String, required: true, trim: true },
-  titik_jemput: { type: String, required: true, trim: true },
-  biaya_jemput: { type: Number, required: true, min: 0 },
-  jarak_jemput: { type: Number, required: true, min: 0 },
-  total_biaya: { type: Number, required: true, min: 0 },
-  status: { 
-    type: String, 
-    enum: ['waiting payment', 'pickup ongoing', 'done'], 
-    required: true 
+const TransactionSchema = new mongoose.Schema(
+  {
+    id_transaction: { type: String, required: true, unique: true },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    warehouse_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Warehouse",
+      required: true,
+    },
+    barang_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Barang",
+      required: true,
+    },
+    titik_alamat: { type: String, required: true, trim: true },
+    titik_jemput: { type: String, required: true, trim: true },
+    biaya_jemput: { type: Number, required: true, min: 0 },
+    jarak_jemput: { type: Number, required: true, min: 0 },
+    total_biaya: { type: Number, required: true, min: 0 },
+    status: {
+      type: String,
+      enum: ["waiting payment", "pickup ongoing", "done"],
+      required: true,
+    },
+    denda: { type: Number, default: 0, min: 0 },
   },
-  denda: { type: Number, default: 0, min: 0 }
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
