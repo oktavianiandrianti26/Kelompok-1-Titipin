1cd7ccca8979dae295c571e707e738af47a52ac6
Alia <absentugasalia@gmail.com>
2024-12-13 15:07:52 +0700

    update tambah ulasan dan balasan

--- backend/src/models/transaction_model.js
+++ backend/src/models/transaction_model.js
@@ -1,38 +1,30 @@
 const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema(
-  {
-    id_transaction: { type: String, required: true, unique: true },
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    warehouse_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Warehouse",
-      required: true,
-    },
-    barang_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Barang",
-      required: true,
-    },
-    titik_alamat: { type: String, required: true, trim: true },
-    titik_jemput: { type: String, required: true, trim: true },
-    biaya_jemput: { type: Number, required: true, min: 0 },
-    jarak_jemput: { type: Number, required: true, min: 0 },
-    total_biaya: { type: Number, required: true, min: 0 },
-    status: {
-      type: String,
-      enum: ["waiting payment", "pickup ongoing", "done"],
-      required: true,
-    },
-    denda: { type: Number, default: 0, min: 0 },
+const TransactionSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  warehouse_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Warehouse', required: true },
+  barang_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Barang', required: true },
+  titik_alamat: { type: String, required: true, trim: true },
+  titik_jemput: { type: String, required: true, trim: true },
+  biaya_jemput: { type: Number, required: true, min: 0 },
+  jarak_jemput: { type: Number, required: true, min: 0 },
+  total_biaya: { type: Number, required: true, min: 0 },
+  status: { 
+    type: String, 
+    enum: ['waiting payment', 'pickup ongoing', 'done'], 
+    required: true 
   },
-  {
-    timestamps: true,
-  }
-);
+  denda: { type: Number, default: 0, min: 0 },
+  ulasan: {
+    type: String,
+    default: null, // Default ulasan menjadi null
+  },
+  balasan: {
+    type: String,  // Field untuk menyimpan balasan
+    default: "",
+  },
+}, {
+  timestamps: true
+});
 
-module.exports = mongoose.model("Transaction", TransactionSchema);
+module.exports = mongoose.model('Transaction', TransactionSchema);
