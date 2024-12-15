c3f4ed053ec31adb86e6d78b36f7dbae288be448
briskaanandaa <briskaananda103@gmail.com>
2024-12-15 23:47:56 +0700

    Menambahkan Riwayat Penitipan User

--- backend/src/controllers_user/riwayatPenitipanUser_controllers.js
+++ backend/src/controllers_user/riwayatPenitipanUser_controllers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Admin = require("../models/admin_model");
 const Barang = require("../models/barang_model");
 const Feedback = require("../models/feedback_model");
@@ -56,63 +55,3 @@ const updateUserTransactionReview = async (req, res) => {
 };
 
 module.exports = { getUserTransactionHistory, updateUserTransactionReview };
-=======
-const Admin = require("../models/admin_model");
-const Barang = require("../models/barang_model");
-const Feedback = require("../models/feedback_model");
-const Payment = require("../models/payment_model");
-const Transaction = require("../models/transaction_model");
-const User = require("../models/user_model");
-const Warehouse = require("../models/warehouse_model");
-
-
-// Fungsi untuk mendapatkan riwayat transaksi
-const getUserTransactionHistory = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const transactions = await Transaction.find({ user_id: userId })
-      .populate("barang_id", "id_barang berat deskripsi")
-      .populate("warehouse_id", "limit_jarak link_gmap")
-      .exec();
-
-    if (!transactions || transactions.length === 0) {
-      return res.status(404).json({ message: "Tidak Ada Transaksi" });
-    }
-
-    res.status(200).json(transactions);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-// Fungsi untuk memperbarui ulasan transaksi
-const updateUserTransactionReview = async (req, res) => {
-  try {
-    const { transactionId } = req.params;
-    const { ulasan } = req.body;
-
-    const transaction = await Transaction.findById(transactionId);
-    if (!transaction) {
-      return res.status(404).json({ message: "Transaksi tidak ditemukan" });
-    }
-
-    // Update ulasan
-    transaction.ulasan = ulasan;
-    await transaction.save();
-
-    res.status(200).json({ message: "Ulasan berhasil diperbarui", transaction });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-module.exports = { getUserTransactionHistory, updateUserTransactionReview };
->>>>>>> a60a57fcda4e7624551a66b9d27c6a05152c5848
--- backend/src/routes_user/riwayatPenitipanUser_routes.js
+++ backend/src/routes_user/riwayatPenitipanUser_routes.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const {
-  getUserTransactionHistory,
-  updateUserTransactionReview,
-} = require("../controllers_user/riwayatPenitipanUser_controllers");
-
-const router = express.Router();
-
-router.get("/riwayat/:userId", getUserTransactionHistory); //Rute untuk mendapatkan transaksi berdasarkan userId
-router.put("/riwayat/:transactionId", updateUserTransactionReview); // Rute untuk memperbarui ulasan
-
-module.exports = router;
-=======
 const express = require("express");
 const {
   getUserTransactionHistory,
@@ -24,4 +10,3 @@ router.get("/riwayat/:userId", getUserTransactionHistory);
 router.put("/riwayat/:transactionId", updateUserTransactionReview); // Rute untuk memperbarui ulasan
 
 module.exports = router;
->>>>>>> a60a57fcda4e7624551a66b9d27c6a05152c5848
