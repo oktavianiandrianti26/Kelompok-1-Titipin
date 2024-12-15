802c30964807aebef2a8a93cb190d011f8bf92ba
briskaanandaa <briskaananda103@gmail.com>
2024-12-15 23:48:33 +0700

    Memperbaiki sidebar dan routes di App Front-end

--- frontend/src/components/SidebarUser.jsx
+++ frontend/src/components/SidebarUser.jsx
@@ -21,21 +21,59 @@ function SidebarUser() {
   return (
     <>
       <Sidebar>
-        <SidebarItem icon={<FiGrid />} text="Dashboard" route="/user/dashboard" active={location.pathname === "/user/dashboard"} />
-        <SidebarItem icon={<FiPackage />} text="Penitipan" route="/PemesananPenitipan" active={location.pathname === "/PemesananPenitipan"} />
-        <SidebarItem icon={<FiTruck />} text="Pengembalian" route="/user/PengambilanBarang" active={location.pathname === "/user/PengambilanBarang"} />
-        <SidebarItem icon={<FiEye />} text="Status"  />
-        <SidebarItem icon={<FiFile />} text="Riwayat" route="/RiwayatPenitipan" active={location.pathname === "/RiwayatPenitipan"} />
-        <SidebarItem icon={<FiBell />} text="Notifikasi" route="/Notifikasi" active={location.pathname === "/Notifikasi"} />
-        <SidebarItem icon={<FiMessageSquare />} text="Chat" route="/faq" active={location.pathname === "/faq"} />
-        <SidebarItem icon={<FiUser />} text="Profil" route="/EditProfile" active={location.pathname === "/EditProfile"} />
-        <SidebarItem icon={<FiHome />} text="Beranda" beranda  />
-        <SidebarItem icon={<FiLogOut />} text="Keluar" keluar   />
+        <SidebarItem
+          icon={<FiGrid />}
+          text="Dashboard"
+          route="/user/dashboard"
+          active={location.pathname === "/user/dashboard"}
+        />
+        <SidebarItem
+          icon={<FiPackage />}
+          text="Penitipan"
+          route="/PemesananPenitipan"
+          active={location.pathname === "/PemesananPenitipan"}
+        />
+        <SidebarItem
+          icon={<FiTruck />}
+          text="Pengembalian"
+          route="/PengambilanBarang"
+          active={location.pathname === "/PengambilanBarang"}
+        />
+        <SidebarItem
+          icon={<FiEye />}
+          text="Status"
+          route="/PemantauanBarang"
+          active={location.pathname === "/PemantauanBarang"}
+        />
+        <SidebarItem
+          icon={<FiFile />}
+          text="Riwayat"
+          route="/RiwayatPenitipan"
+          active={location.pathname === "/RiwayatPenitipan"}
+        />
+        <SidebarItem
+          icon={<FiBell />}
+          text="Notifikasi"
+          route="/Notifikasi"
+          active={location.pathname === "/Notifikasi"}
+        />
+        <SidebarItem
+          icon={<FiMessageSquare />}
+          text="Chat"
+          route="/faq"
+          active={location.pathname === "/faq"}
+        />
+        <SidebarItem
+          icon={<FiUser />}
+          text="Profil"
+          route="/EditProfile"
+          active={location.pathname === "/EditProfile"}
+        />
+        <SidebarItem icon={<FiHome />} text="Beranda" beranda />
+        <SidebarItem icon={<FiLogOut />} text="Keluar" keluar />
       </Sidebar>
     </>
   );
 }
 
 export default SidebarUser;
-
-
